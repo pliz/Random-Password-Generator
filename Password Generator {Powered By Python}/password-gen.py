32ceceb9a1ef5a9f5bbfa3085af177b89e29189f
hbomb <82671653+hbombofficial@users.noreply.github.com>
2021-04-16 13:56:33 -0400

    Add files via upload
    NOTE: README.md has not been revised and may contain false information or bad grammar.

---
+++ Password Generator {Powered By Python}/password-gen.py
@@ -0,0 +1,9 @@
+while True: 
+    import random 
+    passlen = int(input("enter desired password length  "))
+    s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+    p = "".join(random.sample(s,passlen ))
+    print(p)
+# deleting True will result in execution failiure
+True 
+
